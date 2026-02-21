94040a4bb0e69156dda2580faa04505e0783e22f
Alicja Krupa <mintyasleep@protonmail.com>
2026-02-21 17:19:26 +0000

    notice taps on stations

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import {Text, View, ScrollView, StyleSheet, Dimensions, FlatList, StyleProp, ViewStyle, VirtualizedList} from 'react-native';
+import {Text, View, ScrollView, StyleSheet, Dimensions, FlatList, StyleProp, ViewStyle, VirtualizedList, Alert, NativeTouchEvent, TouchableOpacity} from 'react-native';
 import { SafeAreaView , SafeAreaProvider} from 'react-native-safe-area-context';
 import Svg, {Line, Path, SvgUri, SvgXml} from 'react-native-svg';
-import Mapsvg from './mintytram2b.svg';
+import Mapsvg from './mintytram2c.svg';
 import Tramicon from './tramicon.svg';
 import Tramnorth from './tramicon-north.svg';
 import Tramsouth from './tramicon-south.svg';
@@ -12,6 +12,7 @@ import { TouchableWithoutFeedback } from 'react-native';
 //import { err } from 'react-native-svg/lib/typescript/xml';
 
 const map = require('./coords.json');
+const station_locations = require('./coords_station.json');
 const start_time = new Date().getTime();
 
 type Tram = {
@@ -131,7 +132,8 @@ function getPosition(tram: any) {
   let output:StyleProp<ViewStyle> = {
     position:'absolute',
     top:'0%',
-    left:'0%'
+    left:'0%',
+    pointerEvents:'none'
   };
   let path:Array<string>;
   let percent,length:number;
@@ -204,6 +206,7 @@ function getPosition(tram: any) {
   return output;
 }
 
+
 function Tramout(tram: any, index: number) {
   switch (getDirection(tram)) {
     case 0:
@@ -215,31 +218,49 @@ function Tramout(tram: any, index: number) {
   }
 }
 
+
+
+/*
+function createStations(station_id:string) {
+  let left_w = parseInt(station_locations[station_id][0]) * scaling.width + scaling.woffset;
+  let top_w = parseInt(station_locations[station_id][1]) * scaling.height + scaling.hoffset;
+  
+
+  return <TouchableWithoutFeedback key={station_id} onPress={() => showStationInformation.bind(station_id)}><Tramicon width={20} height={20} style={{position:"absolute", top:top_w,left:left_w}}></Tramicon></TouchableWithoutFeedback>
+}
+*/
+
 const App = () => {
   const [trams, setTrams] = useState([]);
   const [connected, setConnected] = useState(true);
   const [notices, setNotices] = useState([]);
   const [expandNotice, setExpandNotice] = useState(true);
+  const [lastTouch, setLastTouch] = useState<NativeTouchEvent>();
+  const [viewingStation, setViewingStation] = useState("");
+  const [stationInformation, setStationInformation] = useState<any>([]);
   useEffect(() => {
     setInterval(() => {
-      fetch('http://tram.mintyasleep.net:3000/active')
-      .then((res) => res.json())
-      .then((data) => {
-        setTrams(data);
-        setConnected(true);
-        console.log('data fetched successfully')
-      }) 
-      .catch((err) => {
-        setConnected(false);
-        console.log(err.message);
-      });
+      //console.log(viewingStation);
+       {
+        fetch('http://tram.mintyasleep.net:3000/active')
+        .then((res) => res.json())
+        .then((data) => {
+          setTrams(data);
+          setConnected(true);
+          //console.log('data fetched successfully')
+        }) 
+        .catch((err) => {
+          setConnected(false);
+          console.log(err.message);
+        });
+      } 
 
       fetch('http://tram.mintyasleep.net:3000/notice')
       .then((res) => res.json())
       .then((data) => {
         //setNotices(data.filter((notice:string) => !notice.includes("END OF SERVICE") && !notice.includes("TravelSafe")));
         setNotices(data.filter(invalidFilter));
-        console.log('notice fetched successfully')
+        //console.log('notice fetched successfully')
       }) 
       .catch((err) => {
         console.log(err.message);
@@ -250,7 +271,57 @@ const App = () => {
 
 
   const expand = () => {
-    setExpandNotice(!expandNotice)
+    setExpandNotice(!expandNotice);
+  }
+
+  const closeStation = () => {
+    setViewingStation("");
+  }
+
+  const showStationInformation = () => {
+    if (Object.keys(stationInformation).length == 4) {
+      return <View></View>
+    } else {
+      return <View></View>
+    }
+
+  }
+
+  const checkTouch = (evt:NativeTouchEvent) => {
+    if (lastTouch != undefined) {
+      //console.log("  Xdif:" + (evt.locationX-lastTouch.locationX) + " Ydif:" + (evt.locationY-lastTouch.locationY));
+      //console.log("    XY:" + evt.locationX + "," + evt.locationY);
+      //console.log("normXY:" + ((evt.locationX - scaling.woffset) / scaling.width) + "," + ((evt.locationY - scaling.hoffset) / scaling.height));
+      let normX = ((evt.locationX - scaling.woffset) / scaling.width);
+      let normY = ((evt.locationY - scaling.hoffset) / scaling.height);
+      if (((evt.locationX - lastTouch.locationX)**2 + (evt.locationY - lastTouch.locationY)**2) <=25) {
+        console.log("checking");
+        let stationTapped = "";
+        let minDistance = 101;
+        Object.keys(station_locations).forEach(station_id => {
+          let distance = ((normX - parseFloat(station_locations[station_id][0]))**2 + (normY - parseFloat(station_locations[station_id][1]))**2);
+          if (distance <=100) {
+            console.log(station_id + distance);
+            if (distance < minDistance) {
+              stationTapped = station_id;
+              minDistance = distance;
+            }
+          }
+        });
+        setViewingStation(stationTapped);
+        console.log('http://tram.mintyasleep.net:3000/stations/' + stationTapped.replaceAll(" ","_"));
+        fetch('http://tram.mintyasleep.net:3000/stations/' + stationTapped.replaceAll(" ","_"))
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setStationInformation(data);
+        }) 
+        .catch((err) => {
+          console.log(err.message);
+        });
+      }
+
+    }
   }
 
   return (
@@ -269,11 +340,18 @@ const App = () => {
         <Text style={{textAlign: 'center',color:"#f0f0f0",display:!expandNotice ? undefined : 'none'}}>{(notices.map(x => "\n" + x + "\n")).join("") + "\n--\n\ntramapp by mintyasleep\ncontact: alicja.work@protonmail.com\n\nContains public sector information licensed under the Open Government Licence v3.0."}</Text>
       </GestureHandlerRootView>
       </TouchableWithoutFeedback>
-      <ScrollView style={{backgroundColor: "#FFF"}}>
+      <ScrollView onTouchStart={(evt) => {setLastTouch(evt.nativeEvent)}} onTouchEnd={(evt) => {checkTouch(evt.nativeEvent)}} style={{backgroundColor: "#FFF", display:viewingStation == "" ? undefined : 'none'}}>
         {/*<Text style={{color:'#000000'}}>{windowDimensions.height},{windowDimensions.width},{windowDimensions.scale}</Text>*/}
         <Mapsvg width={windowDimensions.width} height={windowDimensions.width*6}/>
         {trams.map((item, index) => Tramout(item,index))}
+        {/*Object.keys(station_locations).map((item) => createStations(item))*/}
         {/*<Text style={{color:'black'}}>{JSON.stringify(trams)}</Text>*/}
+      </ScrollView>
+      <ScrollView style={{display:viewingStation != "" ? undefined : 'none'}} onTouchEnd={closeStation}>
+        {showStationInformation()}
+        <Text style={{color:"white"}}>
+          {JSON.stringify(stationInformation)}
+        </Text>
       </ScrollView>
     </SafeAreaView>
     </SafeAreaProvider>
